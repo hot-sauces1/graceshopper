e21dc46173b8d446f40f0d7d6440bf887667d6b8
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-09 17:38:46 -0500

    *removed display id from AllProducts.js *connected singleItem.js to api *added Products link to navbar.js

--- client/components/SingleItem.js
+++ client/components/SingleItem.js
@@ -1,23 +1,22 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {getProductById} from '../store/product'
 
 class SingleItem extends Component {
+  async componentDidMount() {
+    await this.props.fetchSingleProduct(this.props.match.params.id)
+  }
+
   render() {
+    const {singleProduct} = this.props
     return (
       <div>
-        <h1>Title</h1>
-        <img
-          src="https://i.pinimg.com/originals/49/77/9f/49779fdb0331e9671dd974ab2cb300c3.jpg"
-          alt="Ye and Dave Chappelle"
-        />
-        <h3>$29.99</h3>
-        <p>
-          It really matters and then like it really doesn’t matter. What matters
-          is the people who are sparked by it. And the people who are like
-          offended by it, it doesn’t matter. Because it's about motivating the
-          doers. Because I’m here to follow my dreams and inspire other people
-          to follow their dreams, too.
-        </p>
-        <button onClick={() => console.log('YEEZUS JUST ROSE, AGAIN!!!')}>
+        <h1>{singleProduct.name}</h1>
+        <img src={singleProduct.image} alt="Ye and Dave Chappelle" />
+        <h3>${singleProduct.price}</h3>
+        <p>{singleProduct.description}</p>
+        <button onClick={() => alert('YEEZUS JUST ROSE, AGAIN!!!')}>
           Add To Cart
         </button>
       </div>
@@ -25,4 +24,12 @@ class SingleItem extends Component {
   }
 }
 
-export default SingleItem
+const mapStateToProps = state => ({
+  singleProduct: state.product.singleProduct
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchSingleProduct: id => dispatch(getProductById(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleItem)
